80eda098ade6752f089fd0b054c6c3a0ec962b94
shivanpillay20 <91153735+shivanpillay20@users.noreply.github.com>
2024-02-16 06:06:03 +0400

    compare histogram coordinates + top 100 similar

--- project1.py
+++ project1.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-image = cv2.imread('image test 1.png') # image 1
-# image = cv2.imread('image test 2.png') # image 2
+image = cv2.imread('image test 1.png')
 
 if image is None:
     print("Error: Image not loaded.")
@@ -12,11 +11,12 @@ def histogram(x):
     roi = image[x[1]:x[1]+x[3], x[0]:x[0]+x[2]]
     roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     hist = cv2.calcHist([roi_gray], [0], None, [256], [0, 256])
-    plt.plot(hist)
-    plt.title('Histogram')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Frequency')
-    plt.show()
+    return cv2.normalize(hist, hist, 0, 1, cv2.NORM_MINMAX)
+    #plt.plot(hist)
+    #plt.title('Histogram')
+    #plt.xlabel('Pixel Value')
+    #plt.ylabel('Frequency')
+    #plt.show()
 
 
 image1FirstFull=[315,267,125,189]
@@ -36,5 +36,74 @@ image2ThirdFull=[381,269,149,186]
 image2ThirdUpper=[381,269,149,186//2]
 
 
-histogram(image1FirstFull)
-#histogram(image1FirstUpper)
+original_histograms = []
+
+original_histograms.append(histogram(image1FirstFull))
+original_histograms.append(histogram(image1FirstUpper))
+original_histograms.append(histogram(image1SecondFull))
+original_histograms.append(histogram(image1SecondUpper))
+original_histograms.append(histogram(image2FirstFull))
+original_histograms.append(histogram(image2FirstUpper))
+original_histograms.append(histogram(image2SecondFull))
+original_histograms.append(histogram(image2SecondUpper))
+original_histograms.append(histogram(image2ThirdFull))
+original_histograms.append(histogram(image2ThirdUpper))
+
+
+histogram_coordinates = []
+
+histogram_names = [
+    "image1FirstFull", "image1FirstUpper",
+    "image1SecondFull", "image1SecondUpper",
+    "image2FirstFull", "image2FirstUpper",
+    "image2SecondFull", "image2SecondUpper",
+    "image2ThirdFull", "image2ThirdUpper"
+]
+
+with open('labels.txt', 'r') as file:
+    
+    for line in file:
+        values = line.strip().split(',')
+        
+        
+        coordinates = list(map(int, values[-4:]))
+        histogram_coordinates.append((values[0], coordinates))
+
+    
+        
+person_max_value = []
+
+
+
+# Compare histograms and find the maximum intersection for each person
+for i, (filename, coordinates) in enumerate(histogram_coordinates):
+           
+           intersections = [
+            cv2.compareHist(np.float32(histogram(image1FirstFull)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image1FirstUpper)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image1SecondFull)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image1SecondUpper)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image2FirstFull)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image2FirstUpper)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image2SecondFull)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image2SecondUpper)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image2ThirdFull)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+            cv2.compareHist(np.float32(histogram(image2ThirdUpper)),  np.float32(histogram(coordinates)), cv2.HISTCMP_INTERSECT),
+
+        ]
+           #get max value
+           max_intersection = max(intersections)
+           max_histogram_name = histogram_names[intersections.index(max_intersection)]
+           person_max_value.append((i, filename, max_histogram_name, max_intersection,coordinates))
+
+# Store person index, filename, histogram name, and coordinates
+                    
+sorted_person_max_value = sorted(person_max_value, key=lambda x: (x[3],x[4]), reverse=True)
+ # Sort  in descending order
+top_100_people = sorted_person_max_value[:100]
+
+ #  top 100 people 
+print("Top 100 most similar people:")
+for person in top_100_people:
+    print("Filename:", person[1],"Histogram Name:", person[2],"Coordinates:",person[4])
+
